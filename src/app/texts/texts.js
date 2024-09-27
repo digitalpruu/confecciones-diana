d9cf3a1d8a7782573bfb10a6d2c148f2ada0678c
kevinpicon01 <digitalpruu@gmail.com>
2024-09-27 18:16:27 -0500

    Test

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,10 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Confecciones Diana";
+const email = "Tolila19@hotmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "+573115813672";
+
 const textos = {
     index: {
         title: title,
@@ -12,25 +13,26 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En Confecciones Diana, nos especializamos en la confección de ropa para todo tipo de personas. Nuestro compromiso es ofrecer prendas de alta calidad que se adapten a las necesidades y preferencias de nuestros clientes.",
+        description2: "Cada prenda es creada con cuidado y dedicación, asegurando que cada cliente reciba ropa que no solo se vea bien, sino que también sea cómoda y duradera. Confecciones Diana es tu opción ideal si buscas estilo y calidad en una sola prenda."
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Calidad y compromiso en cada prenda.",
+            p2: ""
         },
-        address: "Dirección del emprendimiento",
+        address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/share/aH78wSV5wrBQzS5K/?mibextid=qi2Omg", // Facebook proporcionado
+        instagram: "https://www.instagram.com/aviladianapatricia?igsh=cDZsdDU5bXJ0bzZ2", // Instagram proporcionado
+        tiktok: "https://www.tiktok.com/@dianaavila357?_t=8q3X7EybfHh&_r=1", // TikTok proporcionado
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
+
 export default textos;
